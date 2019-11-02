124a6592ab42807bf574956f1db0b32b03b10d7d
usamausman <usamaa12@gmail.com>
2019-11-02 17:07:51 +0000

    FIx async

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ const App = () => {
     setResults(get)
   }
 
-  const showFlightsFor = async (url) => () => {
+  const showFlightsFor = (url) => async () => {
     setOpen(url)
 
     const data = new FormData(form.current)
